d9125f0c18fb3ce6784388c6a45170f81b83dd56
harman-dhami <86544524+harman-dhami@users.noreply.github.com>
2024-04-15 15:21:29 -0700

    Update pusher.py

--- Shift Match/ShiftMatch/ShiftApp/pusher.py
+++ Shift Match/ShiftMatch/ShiftApp/pusher.py
@@ -1,9 +1,9 @@
 import pusher
 
 pusher_client = pusher.Pusher(
-    app_id='1781407',
-    key='4b5bb58a37e41d8eb66d',
-    secret='4be0810d178aa1aa094c',
-    cluster='us3',
+    app_id='',
+    key='',
+    secret='',
+    cluster='',
     ssl=True
     )
